3642fb9845c6df69d5d89aa4f71864951195879e
Sarwan Kumar <srladuani@gmail.com>
2015-06-29 22:45:41 +0500

    speaker foreign id issue resolved

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,4 @@
 class Speaker < ActiveRecord::Base
-  belongs_to :user, :class => 'User', foreign_id: :speaker_id 
+  belongs_to :user, :class => 'User', foreign_key: :speaker_id 
   belongs_to :conference
 end
